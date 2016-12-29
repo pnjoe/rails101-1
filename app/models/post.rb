c82c6b91ba64916859229fe4f81b5a2f6acf55b7
陈为林 <guangdongjoe@gmail.cm>
2016-12-29 23:32:45 +0800

    implement my groups

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  validates :content, presence: true
+  validates :content, presence: true  #标题不能为空
   scope :recent, -> { order("created_at DESC")}
 end
